60322055f22e6e14fea9bba4837556986fdc0023
Ubuntu <ubuntu@ip-172-31-24-139.us-east-2.compute.internal>
2020-09-16 17:34:14 +0000

    added linters and fixed linters errors

--- src/dom.js
+++ src/dom.js
@@ -1,23 +1,23 @@
-export const pageLoad = () =>{
-    let content = document.getElementById('content');
-    let title = document.createElement('h1');
-    title.className= 'title';
-    title.appendChild(document.createTextNode('AFRO RESTAURANT'));
-    content.appendChild(title);
-    let texOne = document.createElement('p');
-    texOne.className= 'text';
-    texOne.appendChild(document.createTextNode('Afro Deli is an urban fast-casual restaurant that provides a delicious fusion of African, Mediterranean, and American themed food in a fast, fun, and friendly environment'));
-    content.appendChild(texOne);
-    let subtitle = document.createElement('h2');
-    subtitle.className = 'subtitle';
-    subtitle.appendChild(document.createTextNode('We also cater all types of events'));
-    content.appendChild(subtitle);
-    let textTwo = document.createElement('p');
-    textTwo.className = 'text';
-    textTwo.appendChild(document.createTextNode('No matter small or large, we can accomodate your next event. We can create a customized meal that will adhere to every dietary need. Think of us for your next wedding, conference, or other large event'));
-    content.appendChild(textTwo);
-    
-    document.getElementById('footer').innerHTML = `<footer class="page-footer font-small blue">
+export const pageLoad = () => {
+  const content = document.getElementById('content');
+  const title = document.createElement('h1');
+  title.className = 'title';
+  title.appendChild(document.createTextNode('AFRO RESTAURANT'));
+  content.appendChild(title);
+  const texOne = document.createElement('p');
+  texOne.className = 'text';
+  texOne.appendChild(document.createTextNode('Afro Deli is an urban fast-casual restaurant that provides a delicious fusion of African, Mediterranean, and American themed food in a fast, fun, and friendly environment'));
+  content.appendChild(texOne);
+  const subtitle = document.createElement('h2');
+  subtitle.className = 'subtitle';
+  subtitle.appendChild(document.createTextNode('We also cater all types of events'));
+  content.appendChild(subtitle);
+  const textTwo = document.createElement('p');
+  textTwo.className = 'text';
+  textTwo.appendChild(document.createTextNode('No matter small or large, we can accomodate your next event. We can create a customized meal that will adhere to every dietary need. Think of us for your next wedding, conference, or other large event'));
+  content.appendChild(textTwo);
+
+  document.getElementById('footer').innerHTML = `<footer class="page-footer font-small blue">
 
   <!-- Copyright -->
   <div class="footer-copyright text-center py-3">© 2020 Copyright:
@@ -26,5 +26,4 @@ export const pageLoad = () =>{
   <!-- Copyright -->
 
 </footer>`;
-}
-
+};
--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import {pageLoad} from './dom';
-import {contactBot, contactPage} from './contact';
-import {menuBot, menuPage} from './menu';
-let content = document.getElementById('content');
-let homeBot = document.getElementById('home');
+import { pageLoad } from './dom';
+import { contactBot, contactPage } from './contact';
+import { menuBot, menuPage } from './menu';
+
+const content = document.getElementById('content');
+const homeBot = document.getElementById('home');
 
 pageLoad();
 const contPage = () => {
-    content.innerHTML = ""
-    contactPage();
-}
+  content.innerHTML = '';
+  contactPage();
+};
 
 const homePage = () => {
-    content.innerHTML = ""
-    pageLoad();
-}
+  content.innerHTML = '';
+  pageLoad();
+};
 
 const Pagemenu = () => {
-    content.innerHTML = ""
-    menuPage();
-}
+  content.innerHTML = '';
+  menuPage();
+  menuBot.style.color = '#fff';
+};
 
 contactBot.addEventListener('click', contPage);
 homeBot.addEventListener('click', homePage);
